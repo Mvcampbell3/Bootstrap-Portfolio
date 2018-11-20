924ce089de976e8412e5d473823f34745453efef
Mvcampbell3 <mvcampbell3@gmail.com>
2018-11-20 18:34:23 -0500

    updating back to the previous style

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$("#box1").click(function() {
+    console.log("clicked");
+    $("#box1").slideUp("slow");
+});
+
+$("#box2").click(function() {
+    console.log("clicked");
+    $("#box2").slideUp("slow");
+});
+
+$("#box3").click(function() {
+    console.log("clicked");
+    $("#box3").slideUp("slow");
+});
+
+$("#box4").click(function() {
+    console.log("clicked");
+    $("#box4").slideUp("slow");
+});
+
+$("#box5").click(function() {
+    console.log("clicked");
+    $("#box5").slideUp("slow");
+});
+
+
